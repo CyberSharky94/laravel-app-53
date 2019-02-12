1231df7debd378d76ec98401998b81905904b6e2
Lecturer <cybersharky94@gmail.com>
2019-02-12 08:45:38 +0800

    1st day progress

---
+++ resources/views/state/create.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.content')
+
+@section('content_body')
+    
+<form class="form-horizontal">
+        <fieldset>
+        
+        <!-- Form Name -->
+        <legend>Add New State</legend>
+        
+        <!-- Text input-->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="state_name">State Name</label>  
+          <div class="col-md-4">
+          <input id="state_name" name="state_name" type="text" placeholder="State Name" class="form-control input-md" required="">
+            
+          </div>
+        </div>
+        
+        <!-- Text input-->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="state_abbr">State Abbreviation</label>  
+          <div class="col-md-4">
+          <input id="state_abbr" name="state_abbr" type="text" placeholder="State Abbreviation" class="form-control input-md" required="">
+            
+          </div>
+        </div>
+        
+        <!-- Button (Double) -->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="submit"></label>
+          <div class="col-md-8">
+            <button id="submit" name="submit" class="btn btn-primary">Submit</button>
+            <button id="reset" name="reset" class="btn btn-danger">Reset</button>
+          </div>
+        </div>
+        
+        </fieldset>
+        </form>
+        
+
+@endsection
